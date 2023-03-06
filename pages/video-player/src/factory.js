183dca8d207e4319bde589ee1fe8f3e35ef61896
fredowisk <zcrazyhero@gmail.com>
2023-03-06 18:48:06 -0300

    refactor: get correct video path

--- pages/video-player/src/factory.js
+++ pages/video-player/src/factory.js
@@ -39,6 +39,9 @@ async function getWorker() {
 
 const view = new View();
 
+const [rootPath] = window.location.href.split('/pages/')
+view.setVideoSrc(`${rootPath}/assets/video.mp4`)
+
 const worker = await getWorker();
 
 const camera = await Camera.init();
